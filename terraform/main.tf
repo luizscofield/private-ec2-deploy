8d2ee9bd93cecc32050edb292ee3ad4641d92698
Luiz Scofield <luizscofield.pro@gmail.com>
2025-08-22 08:51:25 -0300

    refactor: pipelines

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "./vpc"
+  source = "./modules/vpc"
 
   vpc_name                  = "private-ec2-vpc"
   vpc_cidr_block            = "10.0.0.0/24"
@@ -8,10 +8,16 @@ module "vpc" {
 }
 
 module "ec2" {
-  source = "./ec2"
+  source = "./modules/ec2"
 
   ec2_name      = "private-ec2-instance"
   instance_type = "t2.micro"
   vpc_id        = module.vpc.vpc_id
   subnet_id     = module.vpc.private_subnet_id
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  s3_bucket_name = "sample-python-app-compose"
 }
